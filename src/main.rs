356186a8c6e56f30f303eaabef1f24518df833ea
Stefan Vukanović <stefanvukanovic910@gmail.com>
2022-05-02 11:19:24 +0200

    Show a progress bar and load in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Arg, Command};
 use image::{ImageBuffer, DynamicImage, imageops, io::Reader};
+use indicatif::{ParallelProgressIterator, ProgressBar};
+use rayon::prelude::*;
 
 fn main() {
     let matches = Command::new("Combine frames into sheet")
@@ -34,17 +36,17 @@ fn main() {
     let output = matches.value_of("output").expect("No output specified");
     let rows = matches.value_of("rows").map(| rows | rows.parse::<u32>().unwrap()).unwrap_or(1);
     let columns = matches.value_of("columns").map(| columns | columns.parse::<u32>().unwrap()).unwrap_or(1);
-    let inputs = matches.values_of("inputs").expect("No input files specified");
+    let inputs: Vec::<&str> = matches.values_of("inputs").expect("No input files specified").collect();
 
-    let mut input_files = Vec::<DynamicImage>::new();
+    println!("Loading input images...");
 
-    for input in inputs {
-        println!("Loading image at '{input}'...");
-        input_files.push(Reader::open(input)
-            .expect("Failed to open file.")
-            .decode()
-            .expect("Invalid image file."));
-    }
+    let input_files: Vec::<DynamicImage> = inputs.par_iter().progress_count(inputs.len() as u64)
+        .map(|input| -> DynamicImage {
+            Reader::open(input)
+                .expect("Failed to open file.")
+                .decode()
+                .expect("Invalid image file.")
+        }).collect();
 
     let mut largest_width = 0 as i64;
     let mut largest_height = 0 as i64;
@@ -62,14 +64,23 @@ fn main() {
     let mut x = 0 as i64;
     let mut y = 0 as i64;
 
+    println!("Merging images...");
+
+    let merging_progress = ProgressBar::new(input_files.len() as u64);
+
     for input in &input_files {
         imageops::replace(&mut output_image, input, x * largest_width, y * largest_height);
+        merging_progress.inc(1);
         x = x + 1;
         if x == columns as i64 {
             y = y + 1;
             x = 0;
         }
     }
+
+    merging_progress.finish_and_clear();
+
+    println!("Writing output...");
 
     output_image.save(output).expect("Unable to write to output!");
 }
