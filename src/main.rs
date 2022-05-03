637f68e59907c641c3e946a2169c4bacf209b328
Stefan Vukanović <stefanvukanovic910@gmail.com>
2022-05-03 12:10:01 +0200

    Autoformat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Arg, Command};
-use image::{ImageBuffer, DynamicImage, imageops, io::Reader};
+use image::{imageops, io::Reader, DynamicImage, ImageBuffer};
 use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 
@@ -14,31 +14,37 @@ fn nearest_square(num: u32) -> u32 {
 fn main() {
     let matches = Command::new("Combine frames into sheet")
         .version("0.1.0")
-        .arg(Arg::new("output")
-             .short('o')
-             .long("output")
-             .takes_value(true)
-             .required(true)
-             .help("Output file"))
-        .arg(Arg::new("rows")
-             .short('r')
-             .long("rows")
-             .takes_value(true)
-             .required(true)
-             .help("How many resulting rows should the output sheet have"))
-        .arg(Arg::new("columns")
-             .short('c')
-             .long("columns")
-             .takes_value(true)
-             .required(true)
-             .help("How many resulting columns should the output sheet have"))
-        .arg(Arg::new("inputs")
-             .short('i')
-             .long("inputs")
-             .takes_value(true)
-             .multiple_values(true)
-             .required(true)
-             .help("Input files"))
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .takes_value(true)
+                .required(true)
+                .help("Output file"),
+        )
+        .arg(
+            Arg::new("rows")
+                .short('r')
+                .long("rows")
+                .takes_value(true)
+                .help("How many resulting rows should the output sheet have"),
+        )
+        .arg(
+            Arg::new("columns")
+                .short('c')
+                .long("columns")
+                .takes_value(true)
+                .help("How many resulting columns should the output sheet have"),
+        )
+        .arg(
+            Arg::new("inputs")
+                .short('i')
+                .long("inputs")
+                .takes_value(true)
+                .multiple_values(true)
+                .required(true)
+                .help("Input files"),
+        )
         .get_matches();
 
     let output = matches.value_of("output").expect("No output specified");
@@ -79,15 +85,20 @@ fn main() {
     let bar_style = ProgressStyle::default_bar().template("[{spinner}] [{pos}]/[{len}] {msg}");
     let spinner_style = ProgressStyle::default_bar().template("[{spinner}] {msg}");
 
-    let loading_progress = ProgressBar::new(inputs.len() as u64).with_message("Loading input images").with_style(bar_style.clone());
+    let loading_progress = ProgressBar::new(inputs.len() as u64)
+        .with_message("Loading input images")
+        .with_style(bar_style.clone());
 
-    let input_files: Vec::<DynamicImage> = inputs.par_iter().progress_with(loading_progress)
+    let input_files: Vec<DynamicImage> = inputs
+        .par_iter()
+        .progress_with(loading_progress)
         .map(|input| -> DynamicImage {
             Reader::open(input)
                 .expect("Failed to open file.")
                 .decode()
                 .expect("Invalid image file.")
-        }).collect();
+        })
+        .collect();
 
     let mut largest_width = 0 as i64;
     let mut largest_height = 0 as i64;
@@ -105,10 +116,17 @@ fn main() {
     let mut x = 0 as i64;
     let mut y = 0 as i64;
 
-    let merging_progress = ProgressBar::new(input_files.len() as u64).with_message("Merging images").with_style(bar_style);
+    let merging_progress = ProgressBar::new(input_files.len() as u64)
+        .with_message("Merging images")
+        .with_style(bar_style);
 
     for input in &input_files {
-        imageops::replace(&mut output_image, input, x * largest_width, y * largest_height);
+        imageops::replace(
+            &mut output_image,
+            input,
+            x * largest_width,
+            y * largest_height,
+        );
         merging_progress.inc(1);
         x = x + 1;
         if x == columns as i64 {
@@ -119,7 +137,9 @@ fn main() {
 
     merging_progress.finish_and_clear();
 
-    let saving_progress = ProgressBar::new_spinner().with_message("Writing output").with_style(spinner_style);
+    let saving_progress = ProgressBar::new_spinner()
+        .with_message("Writing output")
+        .with_style(spinner_style);
     saving_progress.enable_steady_tick(15);
 
     output_image.save(output).unwrap();
